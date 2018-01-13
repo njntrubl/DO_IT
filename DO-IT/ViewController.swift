c148b4f969916b63e77c094f28649d638082747c
njntrubl <lujan.marc@gmail.com>
2018-01-13 09:17:18 -0600

    Initial Commit

---
+++ DO-IT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DO-IT
+//
+//  Created by njntrubl on 1/13/18.
+//  Copyright © 2018 njntrublJoe Momma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
