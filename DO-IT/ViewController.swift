332af35bc28e2299807b7352091d9a0ca0bc4bfa
njntrubl <lujan.marc@gmail.com>
2018-01-13 09:29:33 -0600

    new commit

--- DO-IT/ViewController.swift
+++ DO-IT/ViewController.swift
@@ -3,16 +3,29 @@
 //  DO-IT
 //
 //  Created by njntrubl on 1/13/18.
-//  Copyright © 2018 njntrublJoe Momma. All rights reserved.
+//  Copyright © 2018 njntrubl Joe Momma. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Marc"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
